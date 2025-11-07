a1624cafa2189eafb2fee23c9c140beb983df9b4
AshutoshPandey <ashutosh2239@gmail.com>
2025-11-07 19:10:26 +0530

    student routes modified add validation i insert and updated

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import multer  from "multer";
+import multer from "multer";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
 import { checkRole } from "../middlewares/checkRole.middleware.js";
 import { studentValidationSchema } from "../validations/excelFileData.validation.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { getAllStudent , addStudent ,  updateStudent ,  deleteStudent ,  addStudentViaExcel ,  studentProfile } from "../controllers/student.controller.js";
+import {
+  getAllStudent,
+  addStudent,
+  updateStudent,
+  deleteStudent,
+  addStudentViaExcel,
+  studentProfile,
+} from "../controllers/student.controller.js";
 
 const router = express.Router();
 
@@ -13,27 +20,40 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-
-
-
-
-
 // View all students  ,
 router.get("/all", isAuthenticated, checkRole(["admin"]), getAllStudent);
 
-// Add a single student 
-router.post("/add", isAuthenticated, checkRole(["admin"]),validate(studentValidationSchema), addStudent);
+// Add a single student
+router.post(
+  "/add",
+  isAuthenticated,
+  checkRole(["admin"]),
+  validate(studentValidationSchema),
+  addStudent
+);
 
 // Update a student isAuthenticated, checkRole(["admin"])
-router.put("/update/:student_id", isAuthenticated, checkRole(["admin"]),validate(studentValidationSchema) ,   updateStudent);
+router.put(
+  "/update/:student_id",
+  isAuthenticated,
+  checkRole(["admin"]),
+  validate(studentValidationSchema),
+  updateStudent
+);
 
-// Delete a student 
-router.delete("/delete/:student_id", isAuthenticated, checkRole(["admin"]),  deleteStudent);
+// Delete a student
+router.delete("/delete/:student_id", isAuthenticated, checkRole(["admin"]), deleteStudent);
 
 // Add bulk students via Excel
-router.post("/read-excel", isAuthenticated, checkRole(["admin"]),  upload.single('excelFile') ,  addStudentViaExcel);
+router.post(
+  "/read-excel",
+  isAuthenticated,
+  checkRole(["admin"]),
+  upload.single("excelFile"),
+  addStudentViaExcel
+);
 
-//view student profile both admin and student can see 
-router.get("/:student_id",  isAuthenticated, checkRole(["admin" ,  "user"]), studentProfile);
+//view student profile both admin and student can see
+router.get("/:student_id", isAuthenticated, checkRole(["admin", "user"]), studentProfile);
 
 export default router;
